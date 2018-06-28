263a5ae27111b717751e09418e02cdb2f8ef428c
Ajit Bohra <ajit@lubus.in>
2018-06-28 18:57:24 +0530

    Remove unused methods

--- myblock.php
+++ myblock.php
@@ -81,35 +81,6 @@ if ( ! class_exists( 'TomodomoCo_Myblock' ) ) :
 		}
 
 		/**
-		 * Throw error on object clone
-		 *
-		 * The whole idea of the singleton design pattern is that there is a single
-		 * object, therefore we don't want the object to be cloned.
-		 *
-		 * @since  1.0.0
-		 * @access protected
-		 *
-		 * @return void
-		 */
-		public function __clone() {
-			// Cloning instances of the class is forbidden.
-			myblock_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'myblock' ), '1.0' );
-		}
-
-		/**
-		 * Disable unserializing of the class
-		 *
-		 * @since  1.0.0
-		 * @access protected
-		 *
-		 * @return void
-		 */
-		public function __wakeup() {
-			// Unserializing instances of the class is forbidden.
-			myblock_doing_it_wrong( __FUNCTION__, __( 'Cheatin&#8217; huh?', 'myblock' ), '1.0' );
-		}
-
-		/**
 		 * Setup plugin constants
 		 *
 		 * @since  1.0.0
